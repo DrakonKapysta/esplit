ff8c93c6d08de01620ff83395076f831dec1aef8
HibbiBen <SmartBissnes@proton.me>
2025-01-05 21:04:42 +0200

    added base layout with sidebar and created sidebarlink component

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,20 +1,39 @@
-import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
+import { SidebarLink } from "@/components/shared/SidebarLink";
+import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+import { HomeIcon, User } from "lucide-react";
 
 export const Route = createRootRoute({
   component: () => <RootComponent />,
 });
 
+const routeConstants = [
+  {
+    id: "home",
+    path: "/",
+    icon: <HomeIcon size={22} />,
+    text: "Home",
+  },
+  {
+    id: "profile",
+    path: "/profile",
+    icon: <User size={22} />,
+    text: "Profile",
+  },
+];
+
 function RootComponent() {
   return (
-    <main className="container mx-auto min-h-screen flex flex-col ">
-      <header className="min-h-16 flex items-center gap-4">
-        Header
-        <div>
-          <Link to="/">Home</Link>
-        </div>
-      </header>
-      <Outlet />
+    <main className="min-h-screen flex flex-col ">
+      <div className="flex gap-2 border flex-1">
+        <aside className="flex flex-col p-4 max-w-40 w-full gap-4">
+          <h2 className="text-2xl mb-4">Sidebar</h2>
+          {routeConstants.map((route) => (
+            <SidebarLink key={route.id} {...route} />
+          ))}
+        </aside>
+        <Outlet />
+      </div>
       <footer>Footer</footer>
       <TanStackRouterDevtools />
     </main>
